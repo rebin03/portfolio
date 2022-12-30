b799b58ddb879dbbdb836584a4c8b59413ddc47d
rebin03 <muhammedrebin1794@gmail.com>
2022-12-30 21:18:40 +0530

    uploads

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ================ typing animation =============== */
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Student"],
+    typeSpeed:100,
+    BackSpeed:50,
+    loop:true
+})
