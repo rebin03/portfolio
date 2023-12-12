d6e846f65396e197998c0f613a62b3987733a723
rebin03 <muhammedrebin1794@gmail.com>
2023-12-12 14:42:50 +0530

    Update contents, Add projects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* ================ typing animation =============== */
 
 var typed = new Typed(".typing",{
-    strings:["","Student","Web Developer","Mentor"],
+    strings:["","Student","Web Developer","Tech enthusiast","Mentor"],
     typeSpeed:100,
     BackSpeed:50,
     loop:true
